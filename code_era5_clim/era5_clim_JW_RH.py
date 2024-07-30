318dc0f1eddaf5c7145b67f280878e4de385880c
gj5173@partner.kit.edu <gj5173@uc2n266.localdomain>
2024-07-30 23:00:33 +0200

    Add files from server

---
+++ code_era5_clim/era5_clim_JW_RH.py
@@ -0,0 +1,91 @@
+#!/home/hk-project-test-teemleap/ey9908/share/conda/era5_clim/bin/python3.9
+
+#SBATCH --partition=dev_cpuonly
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10000mb
+#SBATCH --job-name=t850_clim
+#SBATCH --constraint=LSDF
+#SBATCH --output=/home/hk-project-test-teemleap/ey9908/output/era5_clim/30day_T850_climatology_era5.log
+
+import xarray as xr
+import sys
+import os
+import numpy as np
+import pickle as pkl
+from matplotlib import pyplot as plt
+from datetime import datetime,timedelta
+
+def newtime(time,tinc):
+    time=str((datetime.strptime(time, '%Y%m%d_%H')+timedelta(hours=int(tinc))).strftime('%Y%m%d_%H'))
+    return time
+
+year_start=1979
+year_end=2020
+
+firstdate="0615_00"
+lastdate="0714_00"
+
+lsdfdir="/lsdf/kit/imk-tro/projects/MOD/Gruppe_Grams"
+outdir="/home/hk-project-test-teemleap/ey9908/tmp"
+
+era5dir="%s/ec.era5" % (lsdfdir)
+
+lonmin=-180.
+lonmax=-60.
+latmin=25.
+latmax=80.
+
+varname="T"
+plev=85000 #Pa
+
+lons=np.arange(lonmin,lonmax,0.5)
+lats=np.arange(latmin,latmax,0.5)
+
+#outdir="%s/test" % (lsdfdir)
+filename_out="%s/clim_%s%s_30day_centered_on_0629_%s-%s_%s-%sdegE_%s-%sdegN_by_grid_point" % (outdir,varname,plev,year_start,year_end,str(lonmin),str(lonmax),str(latmin),str(latmax))
+
+index = 0
+while year_start != year_end:
+        print(year_start,flush=True)
+
+        day="%s%s" % (year_start,firstdate)
+
+        lastday_per_year=newtime("%s%s" % (year_start,lastdate), 24)
+
+        filenames_per_year=[]
+        while day != lastday_per_year:
+
+            month=day[4:6]
+            datadir="%s/%s/%s" % (era5dir,year_start,month)
+
+            filename="Z%s" % (day)
+            T=xr.load_dataset("%s/%s" % (datadir,filename)).to_array().loc[dict(variable="T",plev=plev,lon=lons,lat=lats)].squeeze()
+            Q=xr.load_dataset("%s/%s" % (datadir,filename)).to_array().loc[dict(variable="Q",plev=plev,lon=lons,lat=lats)].squeeze()
+            TD=np.log(Q*plev/100/(287.058/461.520+(1-287.058/461.520)*Q)/6.112)*243.5/(17.67-np.log(Q*plev/100/(287.058/461.520+(1-287.058/461.520)*Q)/6.112))+273.16
+            data_in=np.exp(17.67*(TD-273.16)/(243.5+TD-273.16)-17.67*(T-273.16)/(243.5+T-273.16))*100
+
+            if index == 0:
+                data=data_in
+            else:
+                data=data+data_in
+
+            day=newtime(day,24)
+            index=index+1
+
+        year_start=year_start+1
+
+clim=data/index
+
+data_out=clim.to_dataset(name=varname) #.drop("variable")
+data_out.lon.attrs["_FillValue"]=False
+data_out.lat.attrs["_FillValue"]=False
+data_out.plev.attrs["_FillValue"]=False
+data_out.RH.attrs["_FillValue"]=False
+
+data_out.to_netcdf(filename_out)
+data_out.close()
+print(filename_out)
+
---
+++ code_era5_clim/run_era5_clim.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+####################################################################
+#                                                                  #
+# run_era5_clim.sh                                                 #
+#                                                                  #
+# This script can be used for calculating ERA-5 climatologies from #
+# Z-files in IMK-TRO's data archive.                               #
+#                                                                  #
+# Usage: Change the seven environment variables, save, and then    #
+#        run ./run_era5_clim.sh.                                   #
+#                                                                  #
+# Dependency: era5_clim_JW.py (adapted from Moritz Deinhard)       #
+#                                                                  #
+# Data output: clim_*30day_centered_on_0629_1979-2020* netcdf file #
+#                                                                  #
+# Time period: 1979-2020, 15 June - 14 July                        #
+# Spatial domain: 180° W-60° W; 25° N - 80° N                      #
+# --> can be changed in lines 25-39 in era5_clim_JW.py             #
+#                                                                  #
+# 20 Sep 2023, Jannik Wilhelm, jannik.wilhelm@kit.edu              #
+#                                                                  #
+####################################################################
+
+# Change variable name and pressure level here:
+# =============================================
+
+
+
+VARNAME="Q"        # available variables: "T": temperature, "Z": geopotential height, "U": zonal wind, "V": meridional wind, "Q": specific humidity
+PLEV=$1         # available levels: 10000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000, 85000, 90000 Pa
+# Change path and file names here:
+# ================================
+PYTHONPATH=/home/kit/imk-tro/gj5173/anaconda3/envs/era5_clim/bin/python   # the path to your python binary of the proper conda environment
+LSDFDIR=/lsdf/kit/imk-tro/projects/MOD/Gruppe_Grams                                          # the path to the LSDF ERA-5 archive (do not change!)
+OUTPUTDIR=/home/kit/imk-tro/gj5173/tmp                                                       # the path, where you want to store the output and log file to
+ORISCRIPT=/home/kit/imk-tro/gj5173/era5_clim/era5_clim_JW.py                                       # the path+name to the python script doing the climatology calculations
+PARTITION="single"                                                                           # the name of the SLURM partition (queue) where to run the job (set to: "single")
+
+
+# =======================================================================================
+# Do NOT change the following lines, which will modify and run the script era5_clim_JW.py
+# =======================================================================================
+
+# Copy the original script to the one that gets actually modified
+SCRIPT=${OUTPUTDIR}/era5_clim_JW.py
+cp ${ORISCRIPT} ${SCRIPT}
+
+# Modification of the python script
+sed -i "1s,[^ ]*[^ ],\#\!${PYTHONPATH},1" ${SCRIPT}
+sed -i "3s,[^ ]*[^ ],"--partition=${PARTITION}",2" ${SCRIPT}
+sed -i "9s,[^ ]*[^ ],"--job-name=${VARNAME}_${PLEV}_clim",2" ${SCRIPT}
+sed -i "11s,[^ ]*[^ ],"--output=${OUTPUTDIR}/${VARNAME}_${PLEV}_clim_$(date +%y%m%d%H%M%S).log",2" ${SCRIPT}
+sed -i "31s,[^ ]*[^ ],lsdfdir=\"${LSDFDIR}\",1" ${SCRIPT}
+sed -i "32s,[^ ]*[^ ],outdir=\"${OUTPUTDIR}\",1" ${SCRIPT}
+sed -i "41s,[^ ]*[^ ],varname=\"${VARNAME}\",1" ${SCRIPT}
+sed -i "42s,[^ ]*[^ ],plev=${PLEV},1" ${SCRIPT}
+sed -i "83s,[^ ]*[^ ],data_out.${VARNAME}.attrs[\"_FillValue\"]=False,1" ${SCRIPT}
+wait
+
+# Running the python script
+sbatch ${SCRIPT}
